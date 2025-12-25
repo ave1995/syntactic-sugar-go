2fd2ebaaa1bf045c9e54a4b7aef550636633663d
ave <veselya95@gmail.com>
2025-12-25 12:45:04 +0100

    ALso check in nil map

--- collections/map/main.go
+++ collections/map/main.go
@@ -16,6 +16,7 @@ func LetterFrequencies(word string) {
 
 func WhatHappend() {
 	var m map[string]int
+
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered from panic:", r)
@@ -24,6 +25,9 @@ func WhatHappend() {
 
 	val := m["key"] // safe
 	fmt.Printf("I read from nil map:  %d\n", val)
+
+	val, ok := m["key"]
+	fmt.Printf("I check and read from nil map: %d (ok=%v)\n", val, ok)
 
 	m["key"] = 1
 }
